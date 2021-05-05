05b08ab6de4ed52f7c5cb7638cb402416eb317ba
Xu-xindan <68842260+Xu-xindan@users.noreply.github.com>
2021-05-05 17:07:02 +0800

    Add files via upload

---
+++ 0411Test/src/day.java
@@ -0,0 +1,8 @@
+public class day {
+    public String getDateTimeAferSpecifiedDays (String dateString, String days) {
+        int year=Integer.parseInt(dateString.substring(0,4));
+        int month=Integer.parseInt(dateString.substring(5,7));
+        int day=Integer.parseInt(dateString.substring(8,10));
+        int daysNum=Integer.parseInt(days);
+    }
+}
